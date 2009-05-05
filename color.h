b8ebe3a46241dd4625e59d3f3563bc758a5662f2
Todd Gamblin <tgamblin@llnl.gov>
2009-05-05 15:51:30 +0000

    Adding color.h

---
+++ color.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define None          "\033[0m"
+
+#define Black         "\033[0;30m"
+#define Blue          "\033[0;34m"
+#define Green         "\033[0;32m"
+#define Cyan          "\033[0;36m"
+#define Red           "\033[0;31m"
+#define Purple        "\033[0;35m"
+#define Brown         "\033[0;33m"
+#define Light_Gray    "\033[0;37m"
+#define Dark_Gray     "\033[1;30m"
+#define Light_Blue    "\033[1;34m"
+#define Light_Green   "\033[1;32m"
+#define Light_Cyan    "\033[1;36m"
+#define Light_Red     "\033[1;31m"
+#define Light_Purple  "\033[1;35m"
+#define Yellow        "\033[1;33m"
+#define White         "\033[1;37m"
+
+#endif // COLOR_H
