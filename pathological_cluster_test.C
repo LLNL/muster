950bcb03cbfbcbfa2403141e2f9c6c4c1f255d20
Todd Gamblin <tgamblin@llnl.gov>
2009-12-05 15:13:23 -0800

    Added a pathological test case of points on the line y = x. This has multiple clusters of the same quality, and kmedoids would not converge on it due to roundoff error.

---
+++ pathological_cluster_test.C
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+
+#include "kmedoids.h"
+#include "matrix_utils.h"
+#include "point.h"
+#include "Timer.h"
+
+using namespace cluster;
+using namespace std;
+
+
+int main(int argc, char **argv) {
+  size_t max_k = 10;
+  if (argc > 1) {
+    max_k = atoi(argv[1]);
+  }
+
+  MPI_Init(&argc, &argv);
+  
+  int rank, size;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  // make a set of points to cluster in a diagonal line from the origin.
+  std::vector<point> points;
+  for (size_t i=0; i < max_k; i++) {
+    points.push_back(point(i,i));
+  }
+  
+  dissimilarity_matrix distance;
+  build_dissimilarity_matrix(points, point_distance(), distance);
+
+  kmedoids cluster;
+  for (size_t k=1; k <= max_k; k++) {
+    Timer timer;
+    cluster.pam(distance, k);
+    timer.record("Cluster");
+    
+    cerr << k << setw(20) << timer["Cluster"]/1e9 << endl;
+  }
+
+  MPI_Finalize();
+}
