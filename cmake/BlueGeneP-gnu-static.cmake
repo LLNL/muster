5056b94e7a9a97ac95ce01c58a696abdc4b50e36
Todd Gamblin <tgamblin@llnl.gov>
2010-07-05 11:01:07 -0700

    BlueGene/P Toolchain files.

---
+++ cmake/BlueGeneP-gnu-static.cmake
@@ -0,0 +1,22 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME BlueGeneP-static)
+
+# Set search paths to prefer local, admin-installed wrappers for the BG backend compilers
+set(BGP_GNU_COMPILER_SEARCH_PATHS
+  /usr/local/bin
+  /usr/bin
+  /bgsys/drivers/ppcfloor/gnu-linux/bin)
+
+# GNU C Compilers
+find_program(CMAKE_C_COMPILER       powerpc-bgp-linux-gcc      ${BGP_GNU_COMPILER_SEARCH_PATHS})
+find_program(CMAKE_CXX_COMPILER     powerpc-bgp-linux-g++      ${BGP_GNU_COMPILER_SEARCH_PATHS})
+find_program(CMAKE_Fortran_COMPILER powerpc-bgp-linux-gfortran ${BGP_GNU_COMPILER_SEARCH_PATHS})
+
+# Make sure MPI_COMPILER wrapper matches the gnu compilers.  
+# Prefer local machine wrappers to driver wrappers here too.
+find_program(MPI_COMPILER NAMES mpicxx mpic++ mpiCC mpicc 
+  PATHS 
+  /usr/local/bin
+  /usr/bin
+  /bgsys/drivers/ppcfloor/comm/bin
+  /bgsys/drivers/ppcfloor/comm/default/bin)
---
+++ cmake/BlueGeneP-xl.cmake
@@ -0,0 +1,19 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME BlueGeneP)
+
+# Set search paths to prefer local, admin-installed wrappers for the BG backend compilers
+set(BGP_XL_COMPILER_SEARCH_PATHS /usr/local/bin /usr/bin)
+
+# GNU C Compilers
+find_program(CMAKE_C_COMPILER       bgxlc    ${BGP_XL_COMPILER_SEARCH_PATHS} /opt/ibmcmp/vac/bg/9.0/bin)
+find_program(CMAKE_CXX_COMPILER     bgxlC    ${BGP_XL_COMPILER_SEARCH_PATHS} /opt/ibmcmp/vacpp/bg/9.0/bin)
+find_program(CMAKE_Fortran_COMPILER bgxlf90  ${BGP_XL_COMPILER_SEARCH_PATHS} /opt/ibmcmp/xlf/bg/11.1/bin)
+
+# Make sure MPI_COMPILER wrapper matches the gnu compilers.  
+# Prefer local machine wrappers to driver wrappers here too.
+find_program(MPI_COMPILER NAMES mpixlcxx mpixlc++ mpixlC mpixlc
+  PATHS 
+  /usr/local/bin
+  /usr/bin
+  /bgsys/drivers/ppcfloor/comm/bin
+  /bgsys/drivers/ppcfloor/comm/default/bin)
